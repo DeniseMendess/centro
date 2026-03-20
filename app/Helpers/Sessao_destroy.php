8f8c4e8e41e8d47311a097402db13284e6f22030
Denise Mendes <denisemendesjanota@gmail.com>
2026-03-20 02:38:41 +0100

    feat: cria o login, inicia e destrói a sesão do usuario

--- app/Helpers/Sessao_destroy.php
+++ app/Helpers/Sessao_destroy.php
@@ -4,5 +4,33 @@ namespace app\Helpers;
 
 class Sessao_destroy
 {
+  public static function logado()
+  {
+    if (!isset($_SESSION['usuario_id'])) {
+      header('Location: ' . URL . '/login');
+      exit;
+    }
+  }
 
+
+  public static function somenteAdmin()
+  {
+    self::logado();
+
+    if (!isset($_SESSION['usuario_id']) || $_SESSION['tipo_usuario'] != ADMIN) {
+      header('Location: ' . URL . '/login');
+      exit;
+    }
+  }
+
+
+  public static function somenteFuncionario()
+  {
+    self::logado();
+
+    if ($_SESSION['tipo_usuario'] != FUNCIONARIO) {
+      header('Location: ' . URL . '/login');
+      exit;
+    }
+  }
 }
